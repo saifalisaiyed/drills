6da8d4eb97e26d86adbe73ce3720f323d535cec6
unknown <CCLAYTO@P08791.westfrasertimber.ca>
2015-09-04 11:38:26 -0700

    ADDING FILES

---
+++ drills_files/drills.js
@@ -0,0 +1,137 @@
+
+var interval;
+
+var actions;
+var intervals; 
+var workoutLength;
+
+$("body").ready(function () {
+
+    if (!'speechSynthesis' in window)
+        alert("Sorry, speech synthesis is not supported. Try using Chrome.");
+
+    loadDefaults("#actions", [
+        "1 2 3 2 1",
+        "1 1 2 5 6",
+        "1 5 2 3 4",
+        "2 3 2 3 slip, 3 2 3 2 slip",
+        "fake jab, throw 2 1 2",
+        "jab, left hook to body, left hook to head, right uppercut, left hook",
+        "jab to head, cross, left hook to body",
+        "double jab, straight right, slip, straight right",
+        "5 2 3 2",
+        "1 2, duck to right, 4 3",
+    ]);
+});
+
+$("#start").click(function () {
+
+    
+    actions = getActions();
+    intervals = getIntervals();
+    workoutLength = +getRoundLength() * 60;
+
+    clearInterval(interval);
+            
+    var duration = 10;
+    var elapsed = 0;
+
+    say("Starting in " + duration + " seconds.");
+
+    var loop = function () {
+        clearInterval(interval);
+        
+        console.log(elapsed + " seconds passed");
+        
+        if (elapsed > workoutLength ){ //|| actions.length === 0) {
+            endRound();
+            return;
+        }
+        
+        duration = +getInterval();
+        elapsed += duration;
+                
+        say(getCommand(duration));
+        interval = setInterval(loop, duration * 1000);
+    }
+    interval = setInterval(loop, duration * 1000);
+    
+});
+
+$("#end").click(function () {
+    window.speechSynthesis.cancel();
+    clearInterval(interval);
+    say("Workout cancelled.");
+});
+
+function say(text) {
+    var msg = new SpeechSynthesisUtterance();
+    var voices = window.speechSynthesis.getVoices();
+    msg.voiceURI = 'native';
+    msg.volume = 1;
+    msg.rate = getSpeechRate();
+    msg.text = text;
+    msg.lang = 'en-US';
+    
+    console.log('"' + text + '"');
+    speechSynthesis.speak(msg);
+}
+
+function getAction() {
+    return actions[Math.floor(Math.random() * actions.length)];
+}
+
+function getInterval() {
+    return intervals[Math.floor(Math.random() * intervals.length)];
+}
+
+function getCommand(time) {
+    var action = getAction();
+    return time + " seconds of " + action;
+}
+
+function endRound() {
+    window.speechSynthesis.cancel();
+    say("Round over. Get some water.");
+}
+
+function loadDefaults(jqueryElement, array) {
+    for (var i = 0; i < array.length; i++)
+        $(jqueryElement).append(array[i] + "\n");
+
+}
+
+function getActions() {
+    return $("#actions").val()
+        .split("\n")
+        // remove empty/null/undefined components
+        .filter(function (e) { return e === 0 || e });
+}
+
+function getIntervals(){
+    return $('input:checkbox:checked').map(function () {
+        return +this.value;
+    }).get();
+}
+
+function getRoundLength(){
+    return +$("#radio :radio:checked").val();
+}
+
+function getSpeechRate(){
+    return +$("#rate").slider("values", 0);
+}
+
+Array.prototype.remove = function () {
+    var what, a = arguments,
+        L = a.length,
+        ax;
+    while (L && this.length) {
+        what = a[--L];
+        while ((ax = this.indexOf(what)) !== -1) {
+            this.splice(ax, 1);
+        }
+    }
+    return this;
+};
+
